d006a1803c53987caa1cdc90aeb5e1950ad33918
txurdi <igor@txurdi.net>
2012-10-09 12:33:53 +0200

    Reestructuración de contenido para prepararlo para añadr nuevo contenido.

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>PRUEBAS EN HTML5</title>
+    </head>
+    <body>
+        <?php
+            echo 'Pruebas en HTML5';
+        ?>
+        <ul>
+            <li>
+                Curso HTML5 con @antxoa. Ejemplos de canvas: <a href="curso_antxoa/canvas/canvas.html">0</a>, <a href="curso_antxoa/canvas/canvas1.html">1</a>, <a href="curso_antxoa/canvas/canvas2.html">2</a>, <a href="curso_antxoa/canvas/canvas3.html">3</a>
+            </li>
+            <li>
+                Canvas: Bola de SocSoft <a href="canvas_bola_socsoft/bola_girando.html">girando</a> y <a href="canvas_bola_socsoft/bola.html">sin girar.
+            </li>
+        </ul>
+    </body>
+</html>
